c49654ee12304292eeac3bdd1e2555a606edd0b5
christopheriilajom <70317794+christopheriilajom@users.noreply.github.com>
2020-11-19 21:30:42 +0800

    Update test.js

--- test.js
+++ test.js
@@ -9,16 +9,17 @@ function submit(){
     if (second < first){
         alert("The second number must be greater than or equal to the first number");
     }
+    else{
+        for (var i = first; i<=second; i++){
+            var product = i*third;
+            result += `<h1 class="display-4">${i} X ${third} = ${product}</h1>`;
+            }
+            document.getElementById("outputs").style.visibility = 'visible';
+        document.getElementById("outputs").innerHTML = result;
+        document.getElementById('clickme').innerHTML = 'Reset';
 
-    for (var i = first; i<=second; i++){
-        var product = i*third;
-        result += `${i} X ${third} = ${product} <br>`;
+        document.getElementById('clickme').onclick = function(){
+        document.getElementById('inputType').innerHTML = refreshDoc;
+            }
     }
-    document.getElementById("listContainer").innerHTML = result;
-    document.getElementById('clickme').innerHTML = 'Reset';
-
-    document.getElementById('clickme').onclick = function(){
-    document.getElementById('inputType').innerHTML = refreshDoc;
-    }
-
 }
