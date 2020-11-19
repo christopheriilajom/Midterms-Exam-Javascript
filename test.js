7a6ccdf17b30837635d5465251e0fef874953678
christopheriilajom <70317794+christopheriilajom@users.noreply.github.com>
2020-11-19 13:43:21 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 
 function submit(){
-    var first = document.getElementById("firstNum").value;
-    var second = document.getElementById("secondNum").value;
-    var third = document.getElementById("thirdNum").value;
+    var first = parseInt(document.getElementById("firstNum").value);
+    var second = parseInt(document.getElementById("secondNum").value);
+    var third = parseInt(document.getElementById("thirdNum").value);
     var result = '';
 
     for (var i = first; i<=second; i++){
-        var product = i * third;
+        var product = i*third;
         result += `${i} * ${third} = ${product} <br>`;
     }
     document.getElementById("inputType").innerHTML = result;
