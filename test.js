487da3750eb15f3e57863d7e5c41b2128ad18ef4
christopheriilajom <70317794+christopheriilajom@users.noreply.github.com>
2020-11-19 17:24:15 +0800

    Update test.js

--- test.js
+++ test.js
@@ -3,11 +3,22 @@ function submit(){
     var first = parseInt(document.getElementById("firstNum").value);
     var second = parseInt(document.getElementById("secondNum").value);
     var third = parseInt(document.getElementById("thirdNum").value);
+    var refreshDoc = document.getElementById('inputType').innerHTML;
     var result = '';
+
+    if (second < first){
+        alert("The second number must be greater than or equal to the first number");
+    }
 
     for (var i = first; i<=second; i++){
         var product = i*third;
-        result += `${i} * ${third} = ${product} <br>`;
+        result += `${i} X ${third} = ${product} <br>`;
     }
-    document.getElementById("inputType").innerHTML = result;
+    document.getElementById("listContainer").innerHTML = result;
+    document.getElementById('clickme').innerHTML = 'Reset';
+
+    document.getElementById('clickme').onclick = function(){
+    document.getElementById('inputType').innerHTML = refreshDoc;
+    }
+
 }
